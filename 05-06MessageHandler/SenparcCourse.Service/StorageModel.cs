83dcd0075355240c353ff211721bb283fb3fe6c8
wechatdeveloper <xuexistar@qq.com>
2020-06-30 21:40:21 +0800

    MessageHandler实现用户不同类型的消息的回复

---
+++ 05-06MessageHandler/SenparcCourse.Service/StorageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenparcCourse.Service
+{
+    public class StorageModel
+    {
+        public bool IsInCmd { get; set; }
+
+        public int CmdCount { get; set; }
+    }
+}
