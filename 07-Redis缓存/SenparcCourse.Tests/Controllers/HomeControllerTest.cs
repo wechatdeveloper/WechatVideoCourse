e7134d7e845143a274ed1e27e2338161399764bd
wechatdeveloper <xuexistar@qq.com>
2020-07-18 16:59:31 +0800

    项目配置使用Redis缓存，模拟并发，使用分页式锁控制并发

---
+++ 07-Redis缓存/SenparcCourse.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenparcCourse;
+using SenparcCourse.Controllers;
+
+namespace SenparcCourse.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        private int totalThread = 100; //计划线程要执行20次
+        private int finishedThread = 0;
+
+        [TestMethod]
+        public void LockTest()
+        { 
+            //开启多个线程
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);//给个线程创建时间
+
+                Thread thread = new Thread(RunSingleLockTest);
+                thread.Start(); 
+            }
+
+            while (finishedThread!= totalThread)
+            {
+                //等待线程执行到100次
+            }
+
+            Console.WriteLine("线程执行完毕:"+totalThread.ToString());
+         
+        }
+         
+        private void RunSingleLockTest()
+        {  
+            HomeController controller = new HomeController();
+
+            ContentResult result = controller.LockTest() as ContentResult;
+
+            Console.WriteLine(result.Content);
+
+            finishedThread++; //记得线程执行的次数
+        }
+
+
+    }
+}
---
+++ 07-Redis缓存/SenparcCourse/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using Senparc.CO2NET.Cache;
+
+namespace SenparcCourse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        //Static 保存在内存中，页面观察数据变化
+        private static int _count;
+
+        public static  int Count
+        {
+            get { Thread.Sleep(50); return _count; }
+
+            set
+            {
+                //线程休眠0.1秒；模拟数据更新处理的时间
+                Thread.Sleep(100); 
+                _count = value;
+            } 
+        } 
+
+        public ContentResult LockTest()
+        {
+            //获取当前缓存实例
+            var strategy = CacheStrategyFactory.GetObjectCacheStrategyInstance();
+            using (strategy.BeginCacheLock("HomeController", "LockTest"))
+            {
+                var count = Count; //数据计出来 
+                Count = count + 1; //数据再更新进去 
+                string strResult = "Count:" + count;
+                return Content(strResult);
+            } 
+        }
+    }
+}
